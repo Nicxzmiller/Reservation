f8bd6babc71fb045d1427d8cd1cdf42e4eca8600
Nicholas Markus <nicxzmiller2@gmail.com>
2022-04-29 13:54:48 +0200

    properties

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Featured.css'
+
+const Featured = () => {
+  return (
+    <div className="featured">
+        <div className="featuredItem">
+            <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x320/115962930.jpg?k=a0ce444e110a4be257f71de4d995ebccb80f9859cb474120a030fda007ff8b71&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Istanbul</h1>
+                <h2>13 Properties</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x320/115962930.jpg?k=a0ce444e110a4be257f71de4d995ebccb80f9859cb474120a030fda007ff8b71&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Istanbul</h1>
+                <h2>13 Properties</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x320/115962930.jpg?k=a0ce444e110a4be257f71de4d995ebccb80f9859cb474120a030fda007ff8b71&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Istanbul</h1>
+                <h2>13 Properties</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x320/115962930.jpg?k=a0ce444e110a4be257f71de4d995ebccb80f9859cb474120a030fda007ff8b71&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Istanbul</h1>
+                <h2>13 Properties</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img src="https://q-xx.bstatic.com/xdata/images/xphoto/300x320/115962930.jpg?k=a0ce444e110a4be257f71de4d995ebccb80f9859cb474120a030fda007ff8b71&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Istanbul</h1>
+                <h2>13 Properties</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
